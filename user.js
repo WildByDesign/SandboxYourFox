ea9e4222e788b5e49cc3bee44012f925eee11301
WildByDesign <wildbydesign@live.ca>
2024-01-22 08:40:42 -0500

    Update user.js for Firefox 122
    Firefox recently added translations built into the browser. Unfortunately, those built-in translations cause Firefox to crash in the AppContainer.
    
    This updated user.js file simply adds prefs to disable the built-in translations to avoid crashing.

---
+++ user.js
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *  AppContainer (LPAC) for Firefox - user.js                                 *
+ *                                                                            *
+ *  The purpose here is to disable settings which can spawn additional        *
+ *  firefox.exe processes which would cause the sandbox to crash.             *
+ ******************************************************************************/
+
+// Set Downloads Directory
+user_pref("browser.download.dir", "C:\\AppContainer\\Downloads");
+// Disable Default Browser Check
+user_pref("browser.shell.checkDefaultBrowser", false);
+user_pref("browser.shell.defaultBrowserCheckCount", 1);
+user_pref("browser.shell.didSkipDefaultBrowserCheckOnFirstRun", true);
+// Disable Launcher Process
+user_pref("browser.launcherProcess.enabled", false);
+// Disable New Process
+user_pref("dom.noopener.newprocess.enabled", false);
+// Disable VR Process
+user_pref("dom.vr.process.enabled", false);
+// Disable geo-location as it spawns WindowsUtils utility process
+user_pref("geo.provider.ms-windows-location", false);
+// Disable GPU Process
+user_pref("layers.gpu-process.enabled", false);
+// Disable Media RDD Process
+user_pref("media.rdd-process.enabled", false);
+// Disable Media Utility Process
+user_pref("media.utility-process.enabled", false);
+// Disable Network Process
+user_pref("network.process.enabled", false);
+// EME Free
+user_pref("media.eme.enabled", false);
+// Firefox 118+: Disable Utility process for Oracle JavaScript
+user_pref("browser.opaqueResponseBlocking.javascriptValidator", false);
+// Disable Picture-in-Picture to avoid crash
+user_pref("media.videocontrols.picture-in-picture.enabled", false);
+// Private Window Separation
+user_pref("browser.privateWindowSeparation.enabled", false);
+// Disable Browser Translations (due to crash)
+user_pref("browser.translations.automaticallyPopup", false);
+user_pref("browser.translations.enable", false);
+user_pref("browser.translations.panelShown", false);
