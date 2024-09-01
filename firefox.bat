a7a169339ce85d08fd608ee69f6062dfd245ee02
WildByDesign <wildbydesign@live.ca>
2024-09-01 09:00:25 -0400

    Delete firefox.bat (not needed)

--- firefox.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-start C:\AppContainer\AppExec\wsudo.exe --appcontainer --lpac --env "MOZ_FORCE_DISABLE_E10S=1" --appid "AppContainer.Launcher" --appx "C:\AppContainer\AppExec\capabilities.xml" "C:\AppContainer\Firefox\bin\firefox.exe" -private-window -profile "C:\AppContainer\Firefox\profile"
